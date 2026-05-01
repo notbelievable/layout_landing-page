55579af07f577d9ceb8f212c4af44b634878cfe6
ALex <sasha.hivuk@gmail.com>
2026-05-01 13:00:46 +0300

    For verification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const body = document.body;
+const menu = document.querySelector('#menu');
+const openBtn = document.querySelector('.icon--menu');
+const closeBtn = document.querySelector('.icon--close');
+const menuLinks = document.querySelectorAll('.nav__link');
+
+openBtn.addEventListener('click', function () {
+  body.classList.add('is-locked');
+});
+
+closeBtn.addEventListener('click', function () {
+  body.classList.remove('is-locked');
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    body.classList.remove('is-locked');
+  });
+});
